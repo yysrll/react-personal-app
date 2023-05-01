3b24a428339de07225f102b92a95cb976e4d3266
yysrll <yusril.uzumaki@gmail.com>
2023-05-02 06:50:38 +0800

    setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      <h1>Hello, React</h1>
+    </div>
+  );
+}
+
+export default App;
